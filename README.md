1051cdd50d17fa654727142dda312592df5f50e6
Ireniushka <irenerengelruiz@gmail.com>
2020-11-14 13:43:24 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# bserrang-ejercicio-4-5eg2bn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/bserrang-ejercicio-4-5eg2bn)
---
+++ script.js
@@ -0,0 +1,20 @@
+// En este ejercicio tenéis que sustituir TODAS las funciones tradicionales por funciones flecha y que siga funcionando igual.
+
+changeBackgroundButton => document.getElementById("changeBackgroundButton");
+
+changeBackgroundButton.addEventListener("click", function() {
+  this.style.backgroundColor = getRandomColor();
+});
+
+function getRandomColor() {
+  const letters = getLetters();
+  let color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function getLetters(){
+  return '0123456789ABCDEF';
+}
